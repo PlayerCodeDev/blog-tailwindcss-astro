74cde2ce1bf89057dd87e9f47fb94a184a271db3
PlayerCodeDev <o.ramirez.carlos@gmail.com>
2025-08-07 22:07:41 -0400

    agregar documentacion a la funcion toggleMenu

--- src/scripts/header-menu.js
+++ src/scripts/header-menu.js
@@ -2,14 +2,18 @@ const btnMenu = document.getElementById('btn-menu');
 const btnClose = document.getElementById('btn-close');
 const menuModal = document.getElementById('menu-modal');
 
+/**
+ * Abre o cierra el menú
+ * @param {boolean} open 
+ */
 function toggleMenu(open) {
     menuModal.classList.toggle('hidden', !open);
     btnClose.setAttribute('aria-expanded', String(open));
 };
 
+// Eventos
 btnMenu.addEventListener('click', () => toggleMenu(true));
 btnClose.addEventListener('click', () => toggleMenu(false));
-
-document.addEventListener('keydown', (e) => {
+document.addEventListener('keydown', e => {
     if (e.key === 'Escape') toggleMenu(false);
 });
